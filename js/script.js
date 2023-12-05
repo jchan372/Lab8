5a334764e450304bc439d204c71c859beddce78a
jchan372 <143012077+jchan372@users.noreply.github.com>
2023-12-04 19:48:03 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function init(){
     function showMeText(){
       var textbox = document.getElementById('entryinput');
       document.getElementById('textoutput').innerHTML = textbox.value;
-      alert("Ayan Chandrasekaran: " + textbox.value);
+      alert("Justin Chan: " + textbox.value);
     }
   
     button.addEventListener('click', showMeText);
